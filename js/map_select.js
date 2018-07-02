030bcd5741c7e6e72e5521a4b3fd72b52d507559
BobH <1551608379@qq.com>
2018-07-02 20:37:37 +0800

    server

---
+++ js/map_select.js
@@ -0,0 +1,103 @@
+var $$ = mdui.JQ;
+var selectItem = "";
+var selectItemID = -1;
+var itemIDmax = 0;
+var maps_list = ["夢路らびりんす","風の源","本当のお母さん","出航のマーチ","Say Something","Wolves","Fade","Bad Apple!"];
+function hasClass( elements,cName ){
+    return !!elements.className.match( new RegExp( "(\\s|^)" + cName + "(\\s|$)") ); // ( \\s|^ ) 判断前面是否有空格 （\\s | $ ）判断后面是否有空格 两个感叹号为转换为布尔值 以方便做判断
+}
+function removeClass( elements,cName ){
+    if( hasClass( elements,cName ) ){
+        elements.className = elements.className.replace( new RegExp( "(\\s|^)" + cName + "(\\s|$)" )," " ); // replace方法是替换
+    };
+}
+function addClass( elements,cName ){
+    if( !hasClass( elements,cName ) ){
+        elements.className += " " + cName;
+    };
+}
+var unActiveAll = ()=>{
+    var cNode =document.getElementById('map_list').getElementsByTagName('li');
+    for( var i=0; i<cNode.length; i++){
+        //console.log(cNode[i]);
+        removeClass(cNode[i],"mdui-list-item-active");
+    }
+}
+var ActiveItem = (item)=>{
+    addClass(item,"mdui-list-item-active");
+}
+function parseDom(arg) {
+
+    　　 var objE = document.createElement("div");
+    
+    　　 objE.innerHTML = arg;
+    
+    　　 return objE.childNodes[0];
+    
+};
+var addItemToList = (songName)=>{
+    /*
+    <li class="mdui-list-item">
+        <i class="mdui-icon material-icons">queue_music</i>
+        <div class="mdui-list-item-content">%%name%%</div>
+    </li>
+    */
+    let str = "<li class='mdui-list-item' item_id='%%id%%'><i class='mdui-icon material-icons'>queue_music</i><div class='mdui-list-item-content'>%%name%%</div></li>"
+    str = str.replace("%%name%%",songName);
+    str = str.replace("%%id%%",itemIDmax++);
+    document.getElementById('map_list').appendChild(parseDom(str));
+}
+var getData = ()=>{
+    //从服务器获得地图数据
+}
+var addItems = ()=>{
+    //添加项目到列表中
+    for(let i=0;i<maps_list.length;i++){
+        addItemToList(maps_list[i]);
+    }
+}
+var GameStart = (id,mapname)=>{
+
+}
+$$(()=>{
+    getData();
+    addItems();
+    $$("#map_list").get(0).onclick = (clickEvent)=>{
+        if(clickEvent.srcElement.nodeName == "LI"){
+            //点到了列表上
+            let tmp = clickEvent.srcElement.children[1].innerHTML;
+            unActiveAll();
+            ActiveItem(clickEvent.srcElement);
+            selectItem = tmp;
+            selectItemID = clickEvent.srcElement.getAttribute("item_id");
+            //console.log(tmp);
+            
+        }else if(clickEvent.srcElement.nodeName == "DIV"){
+            //直接点到了名字上
+            let tmp = clickEvent.srcElement.innerHTML;
+            unActiveAll();
+            ActiveItem(clickEvent.srcElement.parentNode);
+            selectItem = tmp;
+            selectItemID = clickEvent.srcElement.parentNode.getAttribute("item_id");
+            //console.log(tmp);
+        }else if(clickEvent.srcElement.nodeName == "I"){
+            //点到了图标上
+            let tmp = clickEvent.srcElement.parentNode.children[1].innerHTML;
+            unActiveAll();
+            ActiveItem(clickEvent.srcElement.parentNode);
+            selectItem = tmp;
+            selectItemID = clickEvent.srcElement.parentNode.getAttribute("item_id");
+            //console.log(tmp);
+        }
+        //console.log(selectItemID);
+        $$("#title").get(0).innerHTML = selectItem;
+        $$("#title").get(0).style.color = "rgb(16, 72, 255)";
+    };
+    $$("#Start").get(0).onclick = ()=>{
+        if(selectItemID == -1){
+            mdui.alert('请确保选择一个你喜欢的地图然后再开始游戏', '你还没有选择地图');
+        }else{
+            GameStart(selectItemID,selectItem);
+        }
+    }
+})
---
+++ js/z_main.js
@@ -0,0 +1,15 @@
+var $$ = mdui.JQ;
+var int=self.setInterval("colorChange()",1000);
+var R = 0;
+function colorChange(){
+    document.getElementById("title2").style.color = "rgb("+ Math.floor(Math.random()*255) +","+ Math.floor(Math.random()*255) +","+ Math.floor(Math.random()*255) +")";
+    document.getElementById("title3").style.color = "rgb("+ Math.floor(Math.random()*255) +","+ Math.floor(Math.random()*255) +","+ Math.floor(Math.random()*255) +")";
+}
+$$(()=>{
+    $$("#GameStart").get(0).onclick = ()=>{
+        window.location.href = "./map_select.html";
+    }
+})
+var goto = ()=>{
+    window.location.href = "./map_select.html";
+}
